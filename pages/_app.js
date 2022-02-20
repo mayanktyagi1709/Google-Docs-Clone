3572bed74a31ae0c5975975c6ba4abbb679cf28b
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-02-20 22:13:53 +0530

    Created Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "@material-tailwind/react/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
