2d906ef5ba533ad1230725d3c0ae044727efce31
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-03-03 18:03:26 +0530

    Added logout functionality

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,23 @@ import Head from "next/head";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import "@material-tailwind/react/tailwind.css";
-import { Provider } from 'next-auth/client'
+import { Provider } from "next-auth/client";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Provider session={pageProps.session}>
-    <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      />
-    <Component {...pageProps} />
-  </Provider>
-  )
+    <>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </head>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
